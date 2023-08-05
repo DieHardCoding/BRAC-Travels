ab2e305e4215397377ef090707df99bb6f763fa9
nocasa99 <99898452+nocasa99@users.noreply.github.com>
2023-08-06 03:47:31 +0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Search from './Components/Search/Search'
+import Support from './Components/Support/Support'
+import Info from './Components/Info/Info'
+import Lounge from './Components/Lounge/Lounge'
+import Travellers from './Components/Travellers/Travellers'
+import Subcribers from './Components/Subscribers/Subscribers'
+import Footer from './Components/Footer/Footer'
+
 
 const App = () => {
   return (
     <div>
-      App
+     
+      <Navbar />
+      <Home />
+      <Search />
+      {/* <Support />
+      <Info />
+      <Lounge/>
+      <Travellers/>
+      <Subcribers/>
+      <Footer/>  */}
+
+
     </div>
   )
 }
