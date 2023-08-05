2ce6d8bd8c7b5fde49a215e83fa4c3b6d3e29c92
nocasa99 <99898452+nocasa99@users.noreply.github.com>
2023-08-06 03:45:43 +0600

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import video from '../../assets/video.mp4'
+import aeroplane from '../../assets/takeOff.png'
 
 const Home = () => {
     return (
+        
         <div className= 'home flex container'>
             <div className="mainText">
-                <h1>Create Ever-Lasting memories with us </h1>
+                <h1>Create Ever-lasting Memories With Us </h1>
             </div>
             <div className="homeImages flex">
                 <div className="videoDiv">
-                    <video src=""></video>
+                    <video src={video} autoPlay muted loop className='video'></video>
                 </div>
 
-                <img src="" className='plane' />
+                <img src={aeroplane} className='plane' />
             </div>
         </div>
     )
